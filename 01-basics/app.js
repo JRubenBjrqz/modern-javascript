3530881a863844aed997052d7025431163ca6f80
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-12-05 23:48:27 -0700

    Console introduction

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -1,7 +1,17 @@
 
 // console.log(console.log('Hello World'));
 
-let a = 10;
-var b = 10;
- 
-c = a + b;
+let a = 10,
+    b = 20,
+    c = 'Hi ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log('%c Hello World', 'color: blue; font-weight: bold');    
+// console.log( { a } );
+// console.log( { b } );
+// console.log( { c } );
+
+// console.table({ a, b, c, d, x });
