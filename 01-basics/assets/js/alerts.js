6bc0b35ded5b766b120666b2f6c6ec87dab696af
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-12-14 21:41:35 -0700

    Alert, prompt and confirm

---
+++ 01-basics/assets/js/alerts.js
@@ -0,0 +1,9 @@
+
+// alert('Hello World');
+
+// let name = prompt('What is your name?', 'No name');
+// console.log( name );
+// console.log( '****' + name + '****' );
+
+const selection = confirm('Are you sure of delete this?');
+console.log( selection );
