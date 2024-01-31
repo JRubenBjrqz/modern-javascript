860c8a0fcb2ae421ff46a9fe1949862a70ad3c38
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2024-01-30 22:34:28 -0700

    Reserved words

---
+++ 01-basics/assets/js/reserved-words.js
@@ -0,0 +1,13 @@
+//Starting with '_', using '$' or adding numbers is acceptable
+let object$ = 123;
+
+//Use underscores solely for separating numbers
+let price99_99 = 123;
+
+//Lower Camel Case
+let playerOne = 'Fernando';
+
+//Upper Camel Case
+class YearGame {
+
+}
