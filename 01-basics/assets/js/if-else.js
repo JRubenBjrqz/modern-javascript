d626aab77e661768b3f18e9f76f508c80108b543
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2024-02-10 20:39:33 -0700

    If else alternative

--- 01-basics/assets/js/if-else.js
+++ 01-basics/assets/js/if-else.js
@@ -20,5 +20,31 @@ if ( day === 0 ) {
 } else if ( day === 2 ) {
     console.log('Tuesday');
 } else {
-    console.log( 'Other day' );
+    console.log( 'It isnt Monday, Tuesday or Sunday.' );
 }
+
+day = 3;
+
+const week = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+}
+
+const arrayWeek = [
+    'Sunday', 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Tuesday', 
+    'Friday', 
+    'Saturday'
+];
+
+console.log( week[day] || 'Día no definido');
+console.log( arrayWeek[day] || 'Día no definido');
+
